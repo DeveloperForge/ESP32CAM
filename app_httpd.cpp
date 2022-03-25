43f07f3dfb51fa295299be96301fa0807a6af54b
DeveloperForge <ZeroEnna@gmail.com>
2022-03-25 03:06:30 -0400

    Update app_httpd.cpp
    missing comma?

--- app_httpd.cpp
+++ app_httpd.cpp
@@ -577,7 +577,7 @@ static esp_err_t status_handler(httpd_req_t *req){
     p+=sprintf(p, "\"colorbar\":%u,", s->status.colorbar);
     p+=sprintf(p, "\"face_detect\":%u,", detection_enabled);
     p+=sprintf(p, "\"face_enroll\":%u,", is_enrolling);
-    p+=sprintf(p, "\"face_recognize\":%u", recognition_enabled);
+    p+=sprintf(p, "\"face_recognize\":%u,", recognition_enabled);
     p+=sprintf(p, "\"flash\":%u,", flash_enabled);
     *p++ = '}';
     *p++ = 0;
